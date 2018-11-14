ed40d87fa4e4b12bf1b90707c46e7eb24b8651c8
rsahoo <rsahoo@IN-L1397.corp.yodlee.com>
2018-11-14 18:26:56 +0530

    pushing ICML

---
+++ icml/src/main/java/com/ICML/icml/IcmlApplication.java
@@ -0,0 +1,12 @@
+package com.ICML.icml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcmlApplication.class, args);
+	}
+}
